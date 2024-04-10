e10573ad537d230db92b980a9d089752f013ce18
lim0n3 <simonestaffa96@gmail.com>
2024-04-10 13:46:51 +0200

    faucet frame

---
+++ app/api/image/route.tsx
@@ -0,0 +1,165 @@
+import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
+import fs from "fs";
+import { join } from "path";
+import { getRedisClient } from "@/lib/redis";
+
+export interface Network {
+  networkId: string;
+  networkName: string;
+  networkLogo: string;
+  tokenName: string;
+  dripAmount: number;
+  address: string;
+  isERC20: boolean;
+  erc20Address?: string;
+  erc20Decimals?: number;
+  isActive: boolean;
+  balance: string;
+}
+
+const interFontPath = join(process.cwd(), "Inter-Regular.ttf");
+const interFontData = fs.readFileSync(interFontPath);
+
+const interSemiboldFontPath = join(process.cwd(), "Inter-SemiBold.ttf");
+const interSemiboldFontData = fs.readFileSync(interSemiboldFontPath);
+
+export async function GET(req: NextRequest) {
+  const networkId = req.nextUrl.searchParams.get("networkId");
+  const redisClient = await getRedisClient();
+  const cachedSupportedNetworksData = await redisClient.get(
+    "supported-networks"
+  );
+  if (!cachedSupportedNetworksData) {
+    throw new Error("No supported networks found in cache");
+  }
+  const cachedSupportedNetworks = JSON.parse(cachedSupportedNetworksData);
+  const network = cachedSupportedNetworks.supportedNetworks.find(
+    (n: Network) => n.networkId === networkId
+  );
+  if (!network) {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            alignItems: "center",
+            background: "black",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              fontSize: 60,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: "0 120px",
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            {`Invalid network!`}
+          </div>
+        </div>
+      ),
+      {
+        width: 500,
+        height: 500,
+        fonts: [
+          {
+            data: interFontData,
+            name: "Inter-SemiBold.ttf",
+            style: "normal",
+            weight: 400,
+          },
+        ],
+      }
+    );
+  }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          alignItems: "center",
+          background: "white",
+          justifyContent: "center",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          width: "100%",
+          padding: "48px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "16px",
+          }}
+        >
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/greenline/512/check-1024.png"
+            style={{
+              width: "100px",
+              marginBottom: "24px",
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "16px",
+            }}
+          >
+            <img
+              src={network.networkLogo!}
+              style={{
+                width: "40px",
+              }}
+            />
+            <div style={{ fontSize: "20px" }}>{network.networkName}</div>
+          </div>
+          <div style={{ fontSize: "48px", display: "flex" }}>
+            You just received{" "}
+            <div
+              style={{
+                fontFamily: "Inter-SemiBold",
+                display: "flex",
+                marginLeft: "8px",
+              }}
+            >
+              {" "}
+              {network.dripAmount} {network.tokenName}
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 955,
+      height: 500,
+      fonts: [
+        {
+          data: interFontData,
+          name: "Inter-Regular",
+        },
+        {
+          data: interSemiboldFontData,
+          name: "Inter-SemiBold",
+        },
+      ],
+    }
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,87 @@
+import { BASE_URL } from "@/lib/constants";
+import { fetchMetadata } from "frames.js/next";
+import { checksumAddress } from "viem";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ searchParams }: Props) {
+  const metadata = await fetchMetadata(
+    new URL(
+      `/frame/${searchParams.networkId}?txLink=${searchParams.txLink}`,
+      process.env.BASE_URL || "http://localhost:3001"
+    )
+  );
+  return {
+    title: "Faucet Bot",
+    other: {
+      ...metadata,
+    },
+  };
+}
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const { txLink, networkId } = searchParams as any;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-black">
+      <div className="flex flex-col items-center justify-center space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-2">
+          <div className="text-3xl text-center font-black">Faucet Bot</div>
+          <div className="text-lg text-center font-semibold">
+            Bot for requesting faucet token in any network.
+          </div>
+        </div>
+        {txLink && (
+          <div className="flex flex-col items-center justify-center space-y-2">
+            <img
+              src={`${BASE_URL}/api/image?networkId=${networkId}`}
+            />
+            <div>
+              <button className="bg-white rounded-lg text-black p-2">
+                <a target="_blank" href={txLink}>
+                  View transaction
+                </a>
+              </button>
+            </div>
+          </div>
+        )}
+        <div className="flex flex-col mt-8 space-y-4">
+          <div className="flex flex-row space-x-2">
+            <p className="text-center font-medium">
+              <a
+                target="_blank"
+                className="text-red-600"
+                href={`https://converse.xyz/dm/${process.env.PUBLIC_BOT_ADDRESS}`}
+              >
+                Converse
+              </a>
+            </p>
+            <p>•</p>
+            <p className="text-center font-medium">
+              <a
+                target="_blank"
+                className="text-blue-600"
+                href={`https://go.cb-w.com/messaging?address=${process.env.PUBLIC_BOT_ADDRESS}`}
+              >
+                Coinbase Wallet
+              </a>
+            </p>
+          </div>
+
+          <p className="text-center text-sm">
+            Made with ❤️ by{" "}
+            <a className="text-green-500" href="https://builders.garden">
+              builders.garden
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
